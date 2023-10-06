709c3c090e31936203d9887012e7db8164d1e82e
Gabor Szabo <gabor@szabgab.com>
2023-10-06 11:18:22 +0300

    get main page and robots.txt file

--- src/main.rs
+++ src/main.rs
@@ -19,4 +19,36 @@ fn main() {
         println!("Welcome!");
         println!("Processing {}", &args.host);
     }
+
+    process(&args.host);
+
+}
+
+fn process(url: &str) {
+    get_main_page(url);
+    get_robots_txt(url);
+}
+
+fn get_main_page(url: &str) {
+    let res = match reqwest::blocking::get(url) {
+        Ok(res) => res,
+        Err(err) => {
+            println!("Error {}", err);
+            std::process::exit(1);
+        }
+    };
+    println!("{:?}", res.status());
+    //println!("{:?}", res);
+}
+
+fn get_robots_txt(url: &str) {
+    let res = match reqwest::blocking::get(format!("{}robots.txt", url)) {
+        Ok(res) => res,
+        Err(err) => {
+            println!("Error {}", err);
+            std::process::exit(1);
+        }
+    };
+    println!("{:?}", res.status());
+    println!("{:?}", res.text());
 }
