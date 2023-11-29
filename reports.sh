644f41564b1c4749e9becb31c3ee7d499e9814d2
Gabor Szabo <gabor@szabgab.com>
2023-11-29 10:15:05 +0200

    generate reports locally and save them in the site/ folder

--- reports.sh
+++ reports.sh
@@ -1,10 +1,9 @@
-mkdir _site
 
-cargo run -- --host https://site-checker.code-maven.com/
-mv report.html _site/site-checker.code-maven.com.html
+cargo run -- --pages 10 --host https://site-checker.code-maven.com/
+mv report.html site/site-checker.code-maven.com.html
 
-cargo run -- --host https://ssg.code-maven.com/
-mv report.html _site/ssg.code-maven.com.html
+cargo run -- --pages --host https://ssg.code-maven.com/
+mv report.html site/ssg.code-maven.com.html
 
-cargo run -- --host https://izrael.szabgab.com/
-mv report.html _site/izrael.szabgab.com.html
+cargo run -- --pages 10 --host https://izrael.szabgab.com/
+mv report.html site/izrael.szabgab.com.html
