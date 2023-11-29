67b92c7c339fb33b44427da1be0cd683f749d62a
Gabor Szabo <gabor@szabgab.com>
2023-11-29 20:05:06 +0200

    update the shell script to generate the reports

--- reports.sh
+++ reports.sh
@@ -1,9 +1,9 @@
+set -e
 
-cargo run -- --pages 10 --host https://site-checker.code-maven.com/
-mv report.html site/site-checker.code-maven.com.html
+cargo run --bin seo-site-checker -- --host https://site-checker.code-maven.com/  --json site/reports/site-checker.code-maven.com.json --html site/reports/site-checker.code-maven.com.html
 
-cargo run -- --pages --host https://ssg.code-maven.com/
-mv report.html site/ssg.code-maven.com.html
+cargo run --bin seo-site-checker -- --pages 1 --host https://ssg.code-maven.com/ --json site/reports/ssg.code-maven.com.json --html site/reports/ssg.code-maven.com.html
 
-cargo run -- --pages 10 --host https://izrael.szabgab.com/
-mv report.html site/izrael.szabgab.com.html
+cargo run --bin seo-site-checker -- --host https://izrael.szabgab.com/ --json site/reports/izrael.szabgab.com.json --html site/reports/izrael.szabgab.com.html
+
+cargo run --bin seo-site-checker -- --host https://rust.code-maven.com/ --json site/reports/rust.code-maven.com.json --html site/reports/rust.code-maven.com.html
